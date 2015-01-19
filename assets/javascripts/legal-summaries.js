915c337fd067b36a6c7a9d9cfc1aef9c47eb9d04
Zarino Zappia <mail@zarino.co.uk>
2015-01-19 14:55:05 +0000

    truncate legal passages after the first paragraph

---
+++ assets/javascripts/legal-summaries.js
@@ -0,0 +1,29 @@
+(function($) {
+
+  var summarise = function(){
+    var $div = $(this);
+    var $firstP = $('p:first-of-type', $div);
+    var $extraElements = $firstP.nextAll();
+
+    if($extraElements.length > 0){
+
+      if($('html').attr('lang') == 'es'){
+        var readMore = '(Ver mas)';
+      } else {
+        var readMore = '(Read more)';
+      }
+
+      var $moreLink = $('<a>').on('click', function(){
+        $(this).parent().nextAll().show();
+        $(this).remove();
+      }).text(readMore).addClass('read_more');
+
+      $firstP.append(' ').append($moreLink).nextAll().hide();
+
+    }
+  }
+
+  $(function(){
+    $('.human_and_legal > div + div').each(summarise);
+  })
+})($);
