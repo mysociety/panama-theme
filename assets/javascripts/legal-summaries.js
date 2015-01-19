447a6e3dbdb6fcf9500fc10ce1374f162c16dc89
Steven Day <steve@mysociety.org>
2015-01-19 16:07:09 +0000

    Match panama language on === 'en' rather than != 'es'
    Otherwise es-PA throws things out

--- assets/javascripts/legal-summaries.js
+++ assets/javascripts/legal-summaries.js
@@ -8,8 +8,8 @@
     if($extraElements.length > 0){
 
       var readMore = '(Ver mas)';
-      if($('html').attr('lang') != 'es'){
-        var readMore = '(Read more)';
+      if($('html').attr('lang') === 'en'){
+        readMore = '(Read more)';
       }
 
       var $moreLink = $('<a>').on('click', function(){
@@ -20,9 +20,9 @@
       $firstP.append(' ').append($moreLink).nextAll().hide();
 
     }
-  }
+  };
 
   $(function(){
     $('.human_and_legal > div + div').each(summarise);
-  })
+  });
 })($);
